18515663ff23f18b099a2b1b367185164765038d
Ali ÇİÇEK <ali.cicek@mooryazilim.com>
2021-02-07 23:44:59 +0300

    hangfirelib

---
+++ MakeJobWell.BLL/Abstract/IRepository/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeJobWell.BLL.Abstract.IRepository
+{
+    public interface IEmailSender
+    {
+        Task Sender(string firstName, string mail, Guid activationCode);
+    }
+}
