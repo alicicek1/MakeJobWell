85ce6251ec154a1df9740dc62849af35e9c96475
Ali Çiçek <malicicek1@gmail.com>
2021-05-02 18:05:17 +0300

    GenericMapper

---
+++ MakeJobWell.BLL/Concrete/Mapper/Mapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MakeJobWell.BLL.Concrete.Mapper
+{
+    public static class Mapper
+    {
+        public static TResult Map<T, TResult>(T model) where TResult : class, new()
+        {
+            TResult result = new TResult();
+            typeof(T).GetProperties().ToList().ForEach(p =>
+            {
+                PropertyInfo property= typeof(TResult).GetProperty(p.Name);
+                property.SetValue(result,p.GetValue(model));
+            });
+            return result;
+        }
+    }
+}
--- MakeJobWell.Core/DataAccess/Abstract/IRepository.cs
+++ MakeJobWell.Core/DataAccess/Abstract/IRepository.cs
@@ -16,8 +16,5 @@ namespace MakeJobWell.Core.DataAccess.Abstract
         TEntity Get(Expression<Func<TEntity, bool>> filter, params Expression<Func<TEntity, object>>[] includes);
         ICollection<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null, params Expression<Func<TEntity, object>>[] includes);
         ICollection<TEntity> GetTopSix();
-        ValueTask<TEntity> GetByIdAsync(int id);
-        Task AddRaneAsync(IEnumerable<TEntity> entities);
-        void RemoveRange(IEnumerable<TEntity> entities);
     }
 }
